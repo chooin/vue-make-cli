a47727b011094e760ca159b749aa1906209a41c1
zhuning <chooin@126.com>
2018-04-16 18:21:12 +0800

    update: vue-make-config page

---
+++ templates/[util]/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="[replace]">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: '[replace]',
+  data () {
+    return {
+
+    }
+  },
+  created () {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import "./style";
+</style>
