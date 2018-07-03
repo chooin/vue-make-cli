bf88d3b7bd2043b2ace6b6f22e317d741bdee2f7
zhuning <chooin@126.com>
2018-07-03 11:54:39 +0800

    update: prefix -> targetDirectoryPrefix

---
+++ test.sh
@@ -0,0 +1,8 @@
+rm -rf ./src/*
+
+./bin/vue-make page page/index
+./bin/vue-make view view/index
+./bin/vue-make component component
+./bin/vue-make mixin mixin
+./bin/vue-make store-module module
+./bin/vue-make directive directive
